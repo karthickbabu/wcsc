78be35149b0b849eb4a28e79ad6d489bbc1d0411
KayBee <karthickbabu@yahoo.com>
2021-04-03 08:47:59 -0700

    For datetime conversion to local time zone.

---
+++ skyyoga/assets/js/course.js
@@ -0,0 +1,27 @@
+!(function ($) {
+    "use strict";
+
+    // Reset Time
+    $(window).on('load', function () {
+        document.querySelectorAll('.datetime').forEach(function (elem) {
+            // <p class="datetime" from="2021-04-15 06:00" duration="90">Session 1: </p>
+            var indiaTime = elem.getAttribute('from');
+            var durationInMins = elem.getAttribute('duration');
+
+            var timeZone = new Date().toLocaleTimeString('en-us', { timeZoneName: 'short' }).split(' ')[2]
+
+            var fromTimeIndia = moment.tz(indiaTime, "Asia/Kolkata");
+            var fromTimeLocal = moment(fromTimeIndia).local();
+            var timehtml = "<b>" + elem.innerHTML + "</b>";
+            if (timeZone !== "IST") {
+                timehtml += "<br>" + timeZone + " " + fromTimeLocal.format("ll LT") + " to "
+                    + fromTimeLocal.clone().add(durationInMins, 'minutes').format("LT");
+            }
+            timehtml += "<br>" + "IST " + fromTimeIndia.format("ll LT") + " to "
+                + fromTimeIndia.clone().add(durationInMins, 'minutes').format("LT");
+
+            elem.innerHTML = timehtml;
+        })
+    });
+
+})(jQuery);
