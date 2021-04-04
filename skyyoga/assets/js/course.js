70b20a19e2598e0834911ee680113d4c1f071b59
KayBee <karthickbabu@yahoo.com>
2021-04-04 12:08:45 -0700

    Added or in timing.

--- skyyoga/assets/js/course.js
+++ skyyoga/assets/js/course.js
@@ -7,14 +7,17 @@
             var indiaTime = elem.getAttribute('from');
             var durationInMins = elem.getAttribute('duration');
 
+            var timeZoneArea = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
             var timeZone = new Date().toLocaleTimeString('en-us', { timeZoneName: 'short' }).split(' ')[2]
 
             var fromTimeIndia = moment.tz(indiaTime, "Asia/Kolkata");
-            var fromTimeLocal = moment(fromTimeIndia).local();
-            var timehtml = "<br>" + "<b>" + elem.innerHTML + "</b>";
-            if (timeZone !== "IST") {
+            var fromTimeLocal = moment(fromTimeIndia).tz(Intl.DateTimeFormat().resolvedOptions().timeZone);
+
+            var timehtml = "<br>" + "<b>" + elem.innerHTML + " </b>";
+            if (timeZone !== "IST" && timeZone !== "GMT+5:30") {
                 timehtml += "<br>" + fromTimeLocal.format("ll LT") + " to "
-                    + fromTimeLocal.clone().add(durationInMins, 'minutes').format("LT") + " " + timeZone;
+                    + fromTimeLocal.clone().add(durationInMins, 'minutes').format("LT") + " " + timeZone + " Or";
             }
             timehtml += "<br>" + fromTimeIndia.format("ll LT") + " to "
                 + fromTimeIndia.clone().add(durationInMins, 'minutes').format("LT") + " IST ";
