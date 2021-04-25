d27daab1fd20555cd96aa06e8566f5f7f7bed88e
KayBee <karthickbabu@yahoo.com>
2021-04-25 10:07:09 -0700

    Moving SKY Centers data to skyyoga folder.

---
+++ skyyoga/assets/js/center-script.js
@@ -0,0 +1,102 @@
+"use strict";
+
+// json data
+var sky_centers_api = "https://cdn.jsdelivr.net/gh/karthickbabu/wcsc@main/skyyoga/assets/data/sky-centers-by-country.json";
+
+//var sky_centers_api = "./sky-centers-by-country.json";
+var centersList = [];
+
+function fetchSkyCentersTable() {
+  $.ajax({
+    url: sky_centers_api,
+    method: 'GET',
+    cache: true,
+    type: "text/json"
+  })
+    .done(function (evt) {
+      centersList = evt;
+      loadSkyCentersCountries(evt);
+      loadSkyCentersTable(evt);
+    })
+    .fail(function () {
+      alert('Error : Failed to reach API Url or check your connection');
+    })
+    .then(function (evt) {
+    });
+};
+
+function loadSkyCentersCountries(centersByCountry) {
+
+  /*
+  if (centersJsonArr.length > 0) {
+    var centersByCountry = {};
+    for (var i = 0; i < centersJsonArr.length; i++) {
+        var center = centersJsonArr[i];
+
+        var centersArrayForCurrentCountry = centersByCountry[center.Country];
+        if (!centersArrayForCurrentCountry) {
+            centersArrayForCurrentCountry = [];
+        }
+
+        centersArrayForCurrentCountry.push(center);
+        centersByCountry[center.Country] = centersArrayForCurrentCountry;
+    }
+  }*/
+
+  var countries = Object.keys(centersByCountry);
+  for (var j = 0; j < countries.length; j++) {
+    $(".center-country").append('<option value="' + countries[j] + '">' + countries[j] + '</option>');
+  }
+
+}
+
+function loadSkyCentersTable(centersJsonArr) {
+  // Set timeout for lazy loading
+  setTimeout(function () {
+    var tableHtml = "";
+    if (centersJsonArr.length > 0) {
+      tableHtml += '<table class="table table-striped mt32 skycenters-list">'
+        + '<thead>'
+        + '<tr>'
+        + '<th>Name</th>'
+        + '<th>City</th>'
+        + '<th>Address</th>'
+        + '</tr>'
+        + '</thead>'
+        + '<tbody>';
+
+      for (var j = 0; j < centersJsonArr.length; j++) {
+        tableHtml += '<tr>'
+          + '<td>' + centersJsonArr[j].Name + '</td>'
+          + '<td>' + centersJsonArr[j].City + ', ' + centersJsonArr[j].State + '</td>'
+          + '<td>' + centersJsonArr[j].Address + '</td>'
+          + '</tr>';
+      }
+      tableHtml += '</tbody></table>';
+    } else {
+      tableHtml += '<p class="mt32">Please select the country to view the list of SKY Centers in your location.</p>';
+    }
+
+    // Set all content
+    $('.table-skycenters').html(tableHtml);
+  }, 1000);
+}
+
+function setupFilterSkyCentersTable() {
+  $(".search-input").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $(".skycenters-list tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+
+  $('.center-country').change(function () {
+    let selectedCountry = this.options[this.selectedIndex].text;
+    loadSkyCentersTable(centersList[selectedCountry]);
+  });
+}
+
+$(document).ready(function () {
+  fetchSkyCentersTable();
+  setupFilterSkyCentersTable();
+});
